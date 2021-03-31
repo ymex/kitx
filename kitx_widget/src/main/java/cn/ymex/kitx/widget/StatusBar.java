d53c48d769b62196354d8658e299ec6d51af3998
ymex <ymex@foxmail.com>
2021-03-31 09:50:38 +0800

    add statusbar

---
+++ kitx_widget/src/main/java/cn/ymex/kitx/widget/StatusBar.java
@@ -0,0 +1,86 @@
+package cn.ymex.kitx.widget;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+
+class StatusBar extends View {
+
+    public StatusBar(Context context) {
+        super(context);
+        init(context, null, 0, 0);
+    }
+
+    public StatusBar(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs, 0, 0);
+    }
+
+    public StatusBar(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs, defStyleAttr, 0);
+    }
+
+    public StatusBar(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(context, attrs, defStyleAttr, defStyleRes);
+
+    }
+
+    private void init(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        setBackgroundResource(android.R.color.background_dark);
+        if (isInEditMode()){
+            setVisibility(GONE);
+        }
+    }
+
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int width = MeasureSpec.getSize(widthMeasureSpec);
+
+        setMeasuredDimension(width, getStatusBarHeight(getContext()));
+    }
+
+
+
+    protected static int getInternalDimensionSize(Context context, String key) {
+        int result = 0;
+        try {
+            int resourceId = Resources.getSystem().getIdentifier(key, "dimen", "android");
+            if (resourceId > 0) {
+                int sizeOne = context.getResources().getDimensionPixelSize(resourceId);
+                int sizeTwo = Resources.getSystem().getDimensionPixelSize(resourceId);
+
+                if (sizeTwo >= sizeOne) {
+                    return sizeTwo;
+                } else {
+                    float densityOne = context.getResources().getDisplayMetrics().density;
+                    float densityTwo = Resources.getSystem().getDisplayMetrics().density;
+                    float f = sizeOne * densityTwo / densityOne;
+                    return (int) ((f >= 0) ? (f + 0.5f) : (f - 0.5f));
+                }
+            }
+        } catch (Resources.NotFoundException ignored) {
+            return 0;
+        }
+        return result;
+    }
+    /******************************************************************************/
+    /**
+     * 状态栏高度
+     * @param context
+     * @return
+     */
+    public static int getStatusBarHeight(Context context){
+        return getInternalDimensionSize(context,"status_bar_height");
+    }
+
+
+
+}
