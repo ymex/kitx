e82bc5ba0cbf0538fef5a0f67c6774c66a54fc18
ymex <ymex@foxmail.com>
2022-10-08 15:40:09 +0800

    dialog

--- kitx-snippet/src/main/java/cn/ymex/kitx/snippet/view/dialog.kt
+++ kitx-snippet/src/main/java/cn/ymex/kitx/snippet/view/dialog.kt
@@ -2,42 +2,85 @@ package cn.ymex.kitx.snippet.view
 
 import android.app.Activity
 import android.content.Context
+import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
+import androidx.annotation.StyleRes
 import androidx.appcompat.app.AlertDialog
 import cn.ymex.kitx.snippet.R
 
 
+data class DialogPanelAppearance(
+    /**
+     * 对话框的宽
+     */
+    val layoutWidth: Int = ViewGroup.LayoutParams.MATCH_PARENT,
+    /**
+     * 对话框的高
+     */
+    val layoutHeight: Int = ViewGroup.LayoutParams.MATCH_PARENT,
+    /**
+     * 对话框遮罩透明度
+     */
+    val dimAmount: Float = 0.6f,
+    /**
+     * 对话框的主题样式
+     */
+    @StyleRes val themeResId: Int = R.style.dialogPanel
+)
+
+
+fun Context.roundDialogPanelAppearance(): DialogPanelAppearance {
+    val sw = getScreenWidth()
+    val width = sw - 64.dp
+    return DialogPanelAppearance(
+        width,
+        ViewGroup.LayoutParams.WRAP_CONTENT,
+        0.6f,
+        R.style.dialogPanelRound
+    )
+}
+
+fun Context.dialogPanel(
+    view: View,
+    config: DialogPanelAppearance = roundDialogPanelAppearance(),
+    builderCall: (builder: AlertDialog.Builder) -> Unit = {},
+): AlertDialog {
+    return dialogPanel(0, view, config, builderCall)
+}
+
 fun Context.dialogPanel(
     @LayoutRes layout: Int,
-    afterShow: (dialog: AlertDialog) -> Unit,
-    beforeShow: (builder: AlertDialog.Builder) -> Unit = {},
-    fullScreen: Boolean = false
+    config: DialogPanelAppearance = roundDialogPanelAppearance(),
+    builderCall: (builder: AlertDialog.Builder) -> Unit = {},
+): AlertDialog {
+    return dialogPanel(layout, null, config, builderCall)
+}
+
+
+private fun Context.dialogPanel(
+    @LayoutRes layout: Int,
+    view: View?,
+    config: DialogPanelAppearance = roundDialogPanelAppearance(),
+    builderCall: (builder: AlertDialog.Builder) -> Unit = {},
 ): AlertDialog {
     if (this !is Activity) {
         throw IllegalArgumentException("context must is Activity!")
     }
     val builder =
-        AlertDialog.Builder(this, if (fullScreen) R.style.dialogPanel else R.style.dialogPanelRound)
-            .setView(layout)
-    builder.setCancelable(true)
-    beforeShow(builder)
+        AlertDialog.Builder(this, config.themeResId)
+
+    if (view != null) {
+        builder.setView(view)
+    }
+    if (layout != 0){
+        builder.setView(layout)
+    }
+    builder.setCancelable(false)
+    builderCall(builder)
     val dialog = builder.create()
     dialog.window?.setDimAmount(0.6f)
     dialog.show()
-    if (fullScreen) {
-        dialog.window?.setLayout(
-            ViewGroup.LayoutParams.MATCH_PARENT,
-            ViewGroup.LayoutParams.MATCH_PARENT
-        )
-
-    } else {
-        val sw = getScreenWidth()
-//    val width = sw - 64.0f.px
-        val width = sw / 5 * 4
-        dialog.window?.setLayout(width, ViewGroup.LayoutParams.WRAP_CONTENT)
-    }
-
-    afterShow(dialog)
+    dialog.window?.setLayout(config.layoutWidth, config.layoutHeight)
     return dialog
 }
